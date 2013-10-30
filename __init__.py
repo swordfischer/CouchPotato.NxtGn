a50343648b1b26f6ce7e6cae0fe61fa03c4b2ed6
ThomasDK81 <Thomas@ThomasMBAir.local>
2013-10-30 13:57:46 +0100

    fixes subtab

--- __init__.py
+++ __init__.py
@@ -8,8 +8,7 @@ config = [{
     'groups': [
         {
             'tab': 'searcher',
-            'subtab': 'providers',
-            'list': 'torrent_providers',
+            'subtab': 'torrent_providers',
             'name': 'NxtGn',
             'description': 'See <a href="http://nxtgn.org">NxtGn</a>',
             'wizard': True,
